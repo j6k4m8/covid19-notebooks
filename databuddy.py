9f73264f4c116dc50cd65227550af23ca7159463
Jordan Matelsky <j6k4m8@gmail.com>
2021-12-21 13:41:45 -0500

    Add 2020 election

--- databuddy.py
+++ databuddy.py
@@ -1,6 +1,9 @@
+from typing import List, Union
 import pandas as pd
+from functools import cache
 
 
+@cache
 def get_data():
     dfs = []
     page = 0
@@ -16,10 +19,43 @@ def get_data():
     return pd.concat(dfs)
 
 
-def get_votes():
+@cache
+def get_states():
     return pd.read_csv(
         "./data-resources/StateAbbreviations.csv", index_col="Postalcode"
     ).join(
         pd.read_csv("./data-resources/state-electoral-2016.csv", index_col="State"),
         on="State",
     )
+
+
+@cache
+def get_votes(
+    election_year: Union[int, List[int]] = 2020,
+    all_parties: bool = False,
+    pivot_by_state: bool = True,
+) -> pd.DataFrame:
+    """
+    Get the votes for the given election year.
+    """
+    df = pd.read_csv("./data-resources/1976-2020-president.csv")
+    year_inclusion = (
+        df.year == election_year
+        if isinstance(election_year, int)
+        else df.year.isin(election_year)
+    )
+    df = (
+        df[year_inclusion]
+        .groupby(["year", "state", "party_simplified"])
+        .sum()[["candidatevotes"]]
+        .reset_index()
+    )
+    if not all_parties:
+        df = df[df.party_simplified.isin(["DEMOCRAT", "REPUBLICAN"])]
+
+    if not pivot_by_state:
+        return df
+
+    return df.pivot(
+        index=["year", "state"], columns=["party_simplified"], values=["candidatevotes"]
+    ).reset_index()
